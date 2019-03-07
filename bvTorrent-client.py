962ab32daea58e48fc3c850c4a420073d6976cb3
MicDMar <martmic2@bvu.edu>
2019-03-07 13:00:59 -0600

    Finished bvTorrent-client.py foundation.

--- bvTorrent-client.py
+++ bvTorrent-client.py
@@ -37,8 +37,25 @@ def getByteLine(conn):
     index += 1
   return data[:-1]
 
+#Sends a 12-byte control message to the tracker detailing the clients intentions.
+def sendControlMsg(msg, conn):
+  #TODO
+
+#Handles the New Connection Protocol specified by the tracker.
 def handleNewConnection(conn):
   #TODO
+
+#Handles the protocol for updating the clients Bit Mask.
+#Will send the tracker 1's and 0's equal to the number of chunks for the file,
+#terminated by a '\n'.
+def handleUpdateMask(conn):
+  #TODO
+
+#Handles the protocol for updating current connected clients.
+#Will recieve number of clients from the tracker and descriptors of each client.
+def handleClientListRequest(conn):
+  #TODO
+
 
 def main():
 
@@ -49,7 +66,6 @@ def main():
   conn.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
   conn.connect((address, port))
 
-  #Function to handle the [New Connection Protocol]
   handleNewConnection(conn)
   
   print("Finished Downloading!")
